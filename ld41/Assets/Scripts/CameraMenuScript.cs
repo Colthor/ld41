ed567294a3b428c65af354c57c22e5de3c966aab
Colthor <github@colthor.co.uk>
2018-04-21 12:33:42 +0100

    Werewolf implemented
    Villagers win!

--- ld41/Assets/Scripts/CameraMenuScript.cs
+++ ld41/Assets/Scripts/CameraMenuScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraMenuScript : MonoBehaviour {
 	
 	public GUISkin GuiSkin;
-
+    /*
 	void woo()
 	{
 		Debug.Log("Woo!");
@@ -63,5 +63,79 @@ public class CameraMenuScript : MonoBehaviour {
 			this.menu.Disable();
 			menu2.Enable();
 		}
+	}
+    */
+
+    private Menu MainMenu;
+    private Menu MainMenu2; //MainMenu with extra hang gliding
+    private Menu WerewolfMenu = null;
+    private Menu WerewolfMenu2 = null;
+
+    void InitWerewolf()
+    {
+        string PlayerClass = "", Victor = "";
+        if (WerewolfMenu != null) Destroy(WerewolfMenu);
+        if (WerewolfMenu2 != null) Destroy(WerewolfMenu2);
+		WerewolfMenu = this.gameObject.AddComponent<Menu>();
+		WerewolfMenu2 = this.gameObject.AddComponent<Menu>();
+
+        int i = Random.Range(0, 4);
+        switch (i)
+        {
+            case 0:
+                PlayerClass = "the werewolf";
+                Victor = "werewolves";
+                break;
+            case 1:
+                PlayerClass = "the seer";
+                Victor = "villagers";
+                break;
+            default:
+                PlayerClass = "a villager";
+                Victor = "villagers";
+                break;
+        }
+
+        WerewolfMenu.AddButtonItem("You are " + PlayerClass, delegate() { WerewolfMenu.Disable(); WerewolfMenu2.Enable(); });
+        WerewolfMenu2.AddButtonItem("The " + Victor + " win!", delegate () { WerewolfMenu2.Disable(); MainMenu2.Enable(); });
+
+    }
+
+    void StartWerewolf()
+    {
+        InitWerewolf();
+        WerewolfMenu.Enable();
+        MainMenu.Disable();
+        MainMenu2.Disable();
+    }
+
+    void StartFizzBuzz()
+    {
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		MainMenu = this.gameObject.AddComponent<Menu>();
+		MainMenu2 = this.gameObject.AddComponent<Menu>();
+		MainMenu.AddButtonItem("Werewolf", StartWerewolf);
+		MainMenu2.AddButtonItem("Werewolf", StartWerewolf);
+		MainMenu.AddButtonItem("FizzBuzz", StartFizzBuzz);
+		MainMenu2.AddButtonItem("FizzBuzz", StartFizzBuzz);
+
+        MainMenu.GuiSkin = GuiSkin;
+        MainMenu2.GuiSkin = GuiSkin;
+
+        MainMenu.Enable();
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
 	}
 }
