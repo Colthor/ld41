e52fc96338a0837200b34e9a4d0b7559dd6a69fe
Colthor <github@colthor.co.uk>
2018-04-21 13:01:58 +0100

    FizzBuzz!
    FizzBuzz implemented!

--- ld41/Assets/Scripts/CameraMenuScript.cs
+++ ld41/Assets/Scripts/CameraMenuScript.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class CameraMenuScript : MonoBehaviour {
-	
-	public GUISkin GuiSkin;
+
+    public GUISkin GuiSkin;
     /*
 	void woo()
 	{
@@ -76,8 +76,8 @@ public class CameraMenuScript : MonoBehaviour {
         string PlayerClass = "", Victor = "";
         if (WerewolfMenu != null) Destroy(WerewolfMenu);
         if (WerewolfMenu2 != null) Destroy(WerewolfMenu2);
-		WerewolfMenu = this.gameObject.AddComponent<Menu>();
-		WerewolfMenu2 = this.gameObject.AddComponent<Menu>();
+        WerewolfMenu = this.gameObject.AddComponent<Menu>();
+        WerewolfMenu2 = this.gameObject.AddComponent<Menu>();
 
         int i = Random.Range(0, 4);
         switch (i)
@@ -96,7 +96,7 @@ public class CameraMenuScript : MonoBehaviour {
                 break;
         }
 
-        WerewolfMenu.AddButtonItem("You are " + PlayerClass, delegate() { WerewolfMenu.Disable(); WerewolfMenu2.Enable(); });
+        WerewolfMenu.AddButtonItem("You are " + PlayerClass, delegate () { WerewolfMenu.Disable(); WerewolfMenu2.Enable(); });
         WerewolfMenu2.AddButtonItem("The " + Victor + " win!", delegate () { WerewolfMenu2.Disable(); MainMenu2.Enable(); });
 
     }
@@ -109,9 +109,68 @@ public class CameraMenuScript : MonoBehaviour {
         MainMenu2.Disable();
     }
 
+    private Menu FizzBuzzEndMenu = null;
+    private Menu FizzBuzzCurrent = null;
+
+    void LoseFizzBuzz(int num)
+    {
+        if(FizzBuzzEndMenu != null)
+        {
+            FizzBuzzEndMenu.Disable();
+            Destroy(FizzBuzzEndMenu);
+        }
+
+        bool fizz = (num % 3) == 0;
+        bool buzz = (num % 5) == 0;
+        string correct;
+        if(fizz && buzz)
+        {
+            correct = "FizzBuzz";
+        }
+        else
+        {
+            correct = "" + num;
+            if (fizz) correct = "Fizz";
+            if (buzz) correct = "Buzz";
+        }
+
+        FizzBuzzCurrent.Disable();
+        FizzBuzzEndMenu = this.gameObject.AddComponent<Menu>();
+        FizzBuzzEndMenu.AddButtonItem("Nope, you lose, sorry! The correct answer was " + correct + ".", delegate () { FizzBuzzEndMenu.Disable(); MainMenu2.Enable(); });
+
+        FizzBuzzEndMenu.Enable();
+    }
+
+    void PlayFizzBuzz(int num)
+    {
+        if(FizzBuzzCurrent != null)
+        {
+            FizzBuzzCurrent.Disable();
+            Destroy(FizzBuzzCurrent);
+        }
+        FizzBuzzCurrent = this.gameObject.AddComponent<Menu>();
+
+        bool fizz = (num % 3) == 0;
+        bool buzz = (num % 5) == 0;
+
+        FizzBuzzCurrent.AddButtonItem("" + num + "!",
+            delegate () { if (!(fizz || buzz)) { PlayFizzBuzz(num + 1); } else { LoseFizzBuzz(num); }; });
+        FizzBuzzCurrent.AddButtonItem("Fizz!",
+            delegate () { if (fizz) { PlayFizzBuzz(num + 1); } else { LoseFizzBuzz(num); }; });
+        FizzBuzzCurrent.AddButtonItem("Buzz!",
+            delegate () { if (buzz) { PlayFizzBuzz(num + 1); } else { LoseFizzBuzz(num); }; });
+        FizzBuzzCurrent.AddButtonItem("FizzBuzz!",
+            delegate () { if (fizz && buzz) { PlayFizzBuzz(num + 1); } else { LoseFizzBuzz(num); }; });
+
+        FizzBuzzCurrent.Enable();
+    }
+
     void StartFizzBuzz()
     {
 
+        MainMenu.Disable();
+        MainMenu2.Disable();
+        PlayFizzBuzz(1);
     }
 
 
